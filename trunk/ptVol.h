8bd4c327fc7ee3ef16da819cf14d519e07979b0c
andmax <andmax@8a9c450c-d048-0410-a09f-2fa9ee36210d>
2008-03-20 15:15:32 +0000

    minorChanges

--- trunk/ptVol.h
+++ trunk/ptVol.h
@@ -11,7 +11,7 @@
 
 /**
  *   ptVol : defines a class for volume rendering using projected
- *           tetrahedron with partial pre-integration PTINT in GPU
+ *           tetrahedra with partial pre-integration PTINT in GPU
  *
  * C++ header.
  *
@@ -42,7 +42,7 @@ typedef struct _tetCentroid {
 	}
 } tetCentroid;
 
-/// ----------------------------------   appVol   ------------------------------------
+/// -----------------------------------   ptVol   -------------------------------------
 
 /// PT Volume
 
